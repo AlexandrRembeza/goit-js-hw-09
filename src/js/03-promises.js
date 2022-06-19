fde54cac750099abbbbfadb83b59224b07720d51
AlexandrRembeza <99494287+AlexandrRembeza@users.noreply.github.com>
2022-06-20 02:24:58 +0300

    сделал задания 1 и 2, а 3 в процессе

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,56 +2,58 @@ import Notiflix from 'notiflix';
 import 'notiflix/dist/notiflix-3.2.5.min.css';
 
 const formEL = document.querySelector('.form');
-formEL.addEventListener('submit', getFormValues);
+formEL.addEventListener('submit', getFormValuesAndGetMessages);
 
-const formValues = {};
 let timerId = null;
 
-function getFormValues(e) {
+function getFormValuesAndGetMessages(e) {
   e.preventDefault();
+
+  const formValues = {};
   const formData = new FormData(e.currentTarget);
 
   formData.forEach((value, name) => (formValues[name] = Number(value)));
 
   e.currentTarget.reset();
+
+  createPromise(formValues)
+    .then(value => {
+      Notiflix.Notify.success(`${value}`, {
+        width: '400px',
+        position: 'right-top',
+        clickToClose: true,
+        svgSize: '120px',
+        fontSize: '18px',
+      });
+    })
+    .catch(value => {
+      Notiflix.Notify.failure(`${value}`, {
+        width: '400px',
+        position: 'right-top',
+        clickToClose: true,
+        svgSize: '120px',
+        fontSize: '18px',
+      });
+    });
 }
 
 function createPromise({ amount, delay, step }) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       timerId = setInterval(() => {
-        for (let i = 0; i <= amount; i += 1) {
+        for (let i = 1; i <= amount; i += 1) {
           let shouldResolve = Math.random() > 0.3;
-          let newDelay = newDelay + delay;
 
           if (shouldResolve) {
-            resolve(`✅ Fulfilled promise ${amount}${i} in ${newDelay}ms`);
+            resolve(`✅ Fulfilled promise ${i} in ${delay}ms`);
           } else {
-            reject(`❌ Rejected promise ${amount}${i} in ${newDelay}ms`);
+            reject(`❌ Rejected promise ${i} in ${delay}ms`);
           }
+
+          let newDelay = delay;
+          newDelay += delay;
         }
-        clearInterval(timerId);
       }, step);
     }, delay);
   });
 }
-
-createPromise(formValues)
-  .then(value => {
-    Notiflix.Notify.success(`${value}`, {
-      width: '400px',
-      position: 'right-top',
-      clickToClose: true,
-      svgSize: '120px',
-      fontSize: '18px',
-    });
-  })
-  .catch(value => {
-    Notiflix.Notify.failure(`${value}`, {
-      width: '400px',
-      position: 'right-top',
-      clickToClose: true,
-      svgSize: '120px',
-      fontSize: '18px',
-    });
-  });
