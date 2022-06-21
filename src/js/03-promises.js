a3eba5026f958c0d54dcb1cb1448a1e30d962cff
AlexandrRembeza <99494287+AlexandrRembeza@users.noreply.github.com>
2022-06-21 17:20:04 +0300

    сделал задание номер 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -17,62 +17,112 @@ function getFormValuesAndGetMessages(e) {
   e.currentTarget.reset();
 
   createPromise(formValues);
-  // .then(value => {
-  //   Notiflix.Notify.success(`${value}`, {
-  //     width: '400px',
-  //     position: 'right-top',
-  //     clickToClose: true,
-  //     svgSize: '120px',
-  //     fontSize: '18px',
-  //   });
-  // })
-  // .catch(value => {
-  //   Notiflix.Notify.failure(`${value}`, {
-  //     width: '400px',
-  //     position: 'right-top',
-  //     clickToClose: true,
-  //     svgSize: '120px',
-  //     fontSize: '18px',
-  //   });
-  // });
 }
 
+// function createPromise({ amount, delay, step }) {
+//   return setTimeout(() => {
+//     for (let i = 1; i <= amount; i += 1) {
+//       let shouldResolve = Math.random() > 0.3;
+
+//       if (shouldResolve) {
+//         Promise.resolve(`✅ Fulfilled promise ${i} in ${delay}ms`).then(
+//           value => {
+//             setTimeout(() => {
+//               Notiflix.Notify.success(`${value}`, {
+//                 width: '400px',
+//                 position: 'right-top',
+//                 clickToClose: true,
+//                 svgSize: '120px',
+//                 fontSize: '18px',
+//               });
+//             }, step);
+//           }
+//         );
+//       } else {
+//         Promise.reject(`❌ Rejected promise ${i} in ${delay}ms`).catch(
+//           value => {
+//             setTimeout(() => {
+//               Notiflix.Notify.failure(`${value}`, {
+//                 width: '400px',
+//                 position: 'right-top',
+//                 clickToClose: true,
+//                 svgSize: '120px',
+//                 fontSize: '18px',
+//               });
+//             }, step);
+//           }
+//         );
+//       }
+
+//       delay += step;
+//     }
+//   }, delay);
+// }
+
 function createPromise({ amount, delay, step }) {
-  return setTimeout(() => {
-    for (let i = 1; i <= amount; i += 1) {
-      let shouldResolve = Math.random() > 0.3;
+  let count = 1;
+  let timeDelay = delay;
+
+  setTimeout(() => {
+    const shouldResolve = Math.random() > 0.3;
+
+    if (shouldResolve) {
+      Promise.resolve(`✅ Fulfilled promise ${count} in ${timeDelay}ms`).then(
+        showResolveMessage
+      );
+    } else {
+      Promise.reject(`❌ Rejected promise ${count} in ${timeDelay}ms`).catch(
+        showRejectMessage
+      );
+    }
+
+    timeDelay += step;
+    count += 1;
+
+    timerId = setInterval(() => {
+      checkCount(count, amount);
+
+      const shouldResolve = Math.random() > 0.3;
 
       if (shouldResolve) {
-        Promise.resolve(`✅ Fulfilled promise ${i} in ${delay}ms`).then(
-          value => {
-            setTimeout(() => {
-              Notiflix.Notify.success(`${value}`, {
-                width: '400px',
-                position: 'right-top',
-                clickToClose: true,
-                svgSize: '120px',
-                fontSize: '18px',
-              });
-            }, step);
-          }
+        Promise.resolve(`✅ Fulfilled promise ${count} in ${timeDelay}ms`).then(
+          showResolveMessage
         );
       } else {
-        Promise.reject(`❌ Rejected promise ${i} in ${delay}ms`).catch(
-          value => {
-            setTimeout(() => {
-              Notiflix.Notify.failure(`${value}`, {
-                width: '400px',
-                position: 'right-top',
-                clickToClose: true,
-                svgSize: '120px',
-                fontSize: '18px',
-              });
-            }, step);
-          }
+        Promise.reject(`❌ Rejected promise ${count} in ${timeDelay}ms`).catch(
+          showRejectMessage
         );
       }
 
-      delay += step;
-    }
+      timeDelay += step;
+      count += 1;
+    }, step);
   }, delay);
+}
+
+function showResolveMessage(value) {
+  Notiflix.Notify.success(`${value}`, {
+    width: '400px',
+    position: 'right-top',
+    clickToClose: true,
+    svgSize: '120px',
+    fontSize: '18px',
+  });
+}
+
+function showRejectMessage(value) {
+  Notiflix.Notify.failure(`${value}`, {
+    width: '400px',
+    position: 'right-top',
+    clickToClose: true,
+    svgSize: '120px',
+    fontSize: '18px',
+  });
+}
+
+function checkCount(currentCount, amountOfCounts) {
+  if (currentCount >= amountOfCounts) {
+    clearInterval(timerId);
+    return;
+  }
 }
